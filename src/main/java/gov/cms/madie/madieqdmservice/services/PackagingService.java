9f3b4bf4a9c079b6c424a0072370bdaee041e6e8
adongare <ashok.dongare@semanticbits.com>
2023-12-20 16:22:25 -0500

    MAT-6180 move instance vars to method level

--- src/main/java/gov/cms/madie/madieqdmservice/services/PackagingService.java
+++ src/main/java/gov/cms/madie/madieqdmservice/services/PackagingService.java
@@ -18,8 +18,6 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class PackagingService {
   private final TranslationServiceClient translationServiceClient;
-  private final String CQL_DIR = "cql/";
-  private final String RESOURCES_DIR = "resources/";
 
   public byte[] createMeasurePackage(Measure measure, String accessToken) {
     log.info("Creating the measure package for measure [{}]", measure.getId());
@@ -29,12 +27,14 @@ public class PackagingService {
       return new byte[0];
     }
     log.info("Adding measure package artifacts to the measure package");
+    String resourcesDir = "resources/";
+    String cqlDir = "cql/";
     Map<String, byte[]> entries = new HashMap<>();
     for (TranslatedLibrary translatedLibrary : translatedLibraries) {
       String entryName = translatedLibrary.getName() + "-v" + translatedLibrary.getVersion();
-      entries.put(RESOURCES_DIR + entryName + ".json", translatedLibrary.getElmJson().getBytes());
-      entries.put(RESOURCES_DIR + entryName + ".xml", translatedLibrary.getElmXml().getBytes());
-      entries.put(CQL_DIR + entryName + ".cql", translatedLibrary.getCql().getBytes());
+      entries.put(resourcesDir + entryName + ".json", translatedLibrary.getElmJson().getBytes());
+      entries.put(resourcesDir + entryName + ".xml", translatedLibrary.getElmXml().getBytes());
+      entries.put(cqlDir + entryName + ".cql", translatedLibrary.getCql().getBytes());
     }
     ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
     return new ZipUtility().zipEntries(entries, outputStream);
