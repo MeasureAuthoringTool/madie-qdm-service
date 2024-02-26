d24cfd298284e8568763377edd4a4fa932699101
adongare <ashok.dongare@semanticbits.com>
2024-02-26 12:40:15 -0500

    MAT-6224 test cases

--- src/test/java/gov/cms/madie/services/HqmfServiceTest.java
+++ src/test/java/gov/cms/madie/services/HqmfServiceTest.java
@@ -1,20 +1,26 @@
 package gov.cms.madie.services;
 
+import gov.cms.madie.Exceptions.PackagingException;
 import gov.cms.madie.hqmf.HQMFGeneratorFactory;
 import gov.cms.madie.hqmf.dto.MeasureExport;
 import gov.cms.madie.hqmf.qdm_5_6.HQMFGenerator;
 import gov.cms.madie.models.common.ModelType;
 import gov.cms.madie.models.measure.QdmMeasure;
+import jakarta.xml.bind.JAXBException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -25,16 +31,20 @@ class HqmfServiceTest {
   @Mock HQMFGeneratorFactory factory;
 
   @InjectMocks HqmfService hqmfService;
+  private QdmMeasure measure;
 
-  @Test
-  void generateHqmf() throws Exception {
-    QdmMeasure measure =
+  @BeforeEach
+  void setUp() {
+    measure =
         QdmMeasure.builder()
             .id("1")
             .ecqmTitle("test")
             .model(String.valueOf(ModelType.QDM_5_6))
             .build();
+  }
 
+  @Test
+  void generateHqmf() throws Exception {
     when(simpleXmlService.measureToSimpleXml(any(QdmMeasure.class)))
         .thenReturn("<measure></measure>");
 
@@ -45,5 +55,35 @@ class HqmfServiceTest {
 
     String output = hqmfService.generateHqmf(measure);
     assertThat(output, is(equalTo("<QualityMeasureDocument></QualityMeasureDocument>")));
+  }
+
+  @Test
+  void generateHqmfWhenSimpleXmlGenerationFailed() throws Exception {
+    String message = "An issue occurred while generating the simple xml for measure";
+    doThrow(new JAXBException(message))
+        .when(simpleXmlService)
+        .measureToSimpleXml(any(QdmMeasure.class));
+    Exception ex =
+        Assertions.assertThrows(
+            PackagingException.class,
+            () -> hqmfService.generateHqmf(measure),
+            "Exception occurred");
+    assertThat(ex.getMessage(), containsString(message));
+  }
+
+  @Test
+  void generateHqmfWhenHqmfGenerationFailed() throws Exception {
+    when(simpleXmlService.measureToSimpleXml(any(QdmMeasure.class)))
+        .thenReturn("<measure></measure>");
+    String message = "An issue occurred while generating the HQMF for measure";
+    HQMFGenerator generator = mock(HQMFGenerator.class);
+    when(factory.getHQMFGenerator()).thenReturn(generator);
+    doThrow(new Exception(message)).when(generator).generate(any(MeasureExport.class));
+    Exception ex =
+        Assertions.assertThrows(
+            PackagingException.class,
+            () -> hqmfService.generateHqmf(measure),
+            "Exception occurred");
+    assertThat(ex.getMessage(), containsString(message));
   }
 }
