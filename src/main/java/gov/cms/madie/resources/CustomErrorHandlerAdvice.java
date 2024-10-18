128ff75a6d1b910dcd5db573c68592af23691526
mcmcphillips <matt.mcphillips@icf.com>
2024-10-18 10:39:16 -0700

    MAT-7587: Throw hqmServiceException

--- src/main/java/gov/cms/madie/resources/CustomErrorHandlerAdvice.java
+++ src/main/java/gov/cms/madie/resources/CustomErrorHandlerAdvice.java
@@ -1,5 +1,6 @@
 package gov.cms.madie.resources;
 
+import gov.cms.madie.Exceptions.HQMFServiceException;
 import gov.cms.madie.Exceptions.PackagingException;
 import gov.cms.madie.Exceptions.TranslationServiceException;
 import gov.cms.madie.Exceptions.UnsupportedModelException;
@@ -7,12 +8,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
@@ -30,7 +33,13 @@ public class CustomErrorHandlerAdvice {
   Map<String, Object> handleCustomException(WebRequest request) {
     return getErrorAttributes(request, HttpStatus.BAD_REQUEST);
   }
-
+  // Handle HQMFServiceException separately
+  @ExceptionHandler(HQMFServiceException.class)
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  @ResponseBody
+  Map<String, Object> handleHQMFServiceException(WebRequest request) {
+    return getErrorAttributes(request, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
   private Map<String, Object> getErrorAttributes(WebRequest request, HttpStatus httpStatus) {
     ErrorAttributeOptions errorOptions =
         ErrorAttributeOptions.of(ErrorAttributeOptions.Include.MESSAGE);
