a4a522972526f05aba4909b241b17a20c064caa7
Christopher Hubert <christopher.hubert@semanticbits.com>
2025-07-10 10:05:54 -0400

    MAT-8702: add to policy

--- src/main/java/gov/cms/madie/util/HtmlSanitizerUtil.java
+++ src/main/java/gov/cms/madie/util/HtmlSanitizerUtil.java
@@ -6,14 +6,34 @@ import org.jsoup.safety.Safelist;
 
 public class HtmlSanitizerUtil {
   public static String sanitize(String html) {
-    if(StringUtils.isEmpty(html)) {
+    if (StringUtils.isEmpty(html)) {
       return html;
     }
     Safelist safelist =
         Safelist.basic()
-            .addTags("h1", "h2", "h3")
-            .addAttributes("a", "target")
-            .addProtocols("a", "href", "http", "https");
+            .addTags(
+                "p",
+                "strong",
+                "em",
+                "u",
+                "s",
+                "ul",
+                "ol",
+                "li",
+                "br",
+                "table",
+                "tbody",
+                "td",
+                "tfoot",
+                "th",
+                "thead",
+                "tr",
+                "col",
+                "colgroup")
+            .addAttributes("table", "style")
+            .addAttributes("th", "rowspan", "colspan", "style", "colwidth")
+            .addAttributes("td", "rowspan", "colspan", "style", "colwidth")
+            .addAttributes("col", "style");
 
     return Jsoup.clean(html, safelist);
   }
--- src/test/java/gov/cms/madie/util/HtmlSanitizerUtilTest.java
+++ src/test/java/gov/cms/madie/util/HtmlSanitizerUtilTest.java
@@ -6,51 +6,53 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HtmlSanitizerUtilTest {
 
-    @Test
-    void sanitizeValidHtml() {
-        String html = "<h1>Title</h1><p>Paragraph</p><a href=\"http://example.com\" target=\"_blank\">Link</a>";
-        String sanitized = HtmlSanitizerUtil.sanitize(html);
+  @Test
+  void sanitizeValidHtml() {
+    String html =
+        "<h1>Title</h1><p>Paragraph</p><a href=\"http://example.com\" target=\"_blank\">Link</a>";
+    String sanitized = HtmlSanitizerUtil.sanitize(html);
 
-        assertEquals("<h1>Title</h1>\n<p>Paragraph</p>\n<a href=\"http://example.com\" target=\"_blank\" rel=\"nofollow\">Link</a>", sanitized);
-    }
+    assertEquals(
+        "<h1>Title</h1>\n<p>Paragraph</p>\n<a href=\"http://example.com\" target=\"_blank\" rel=\"nofollow\">Link</a>",
+        sanitized);
+  }
 
-    @Test
-    void sanitizeHtmlWithDisallowedTags() {
-        String html = "<script>alert('XSS');</script><h1>Title</h1>";
-        String sanitized = HtmlSanitizerUtil.sanitize(html);
+  @Test
+  void sanitizeHtmlWithDisallowedTags() {
+    String html = "<script>alert('XSS');</script><h1>Title</h1>";
+    String sanitized = HtmlSanitizerUtil.sanitize(html);
 
-        assertEquals("<h1>Title</h1>", sanitized);
-    }
+    assertEquals("<h1>Title</h1>", sanitized);
+  }
 
-    @Test
-    void sanitizeHtmlWithDisallowedAttributes() {
-        String html = "<a href=\"http://example.com\" onclick=\"alert('XSS')\">Link</a>";
-        String sanitized = HtmlSanitizerUtil.sanitize(html);
+  @Test
+  void sanitizeHtmlWithDisallowedAttributes() {
+    String html = "<a href=\"http://example.com\" onclick=\"alert('XSS')\">Link</a>";
+    String sanitized = HtmlSanitizerUtil.sanitize(html);
 
-        assertEquals("<a href=\"http://example.com\" rel=\"nofollow\">Link</a>", sanitized);
-    }
+    assertEquals("<a href=\"http://example.com\" rel=\"nofollow\">Link</a>", sanitized);
+  }
 
-    @Test
-    void sanitizeEmptyHtml() {
-        String html = "";
-        String sanitized = HtmlSanitizerUtil.sanitize(html);
+  @Test
+  void sanitizeEmptyHtml() {
+    String html = "";
+    String sanitized = HtmlSanitizerUtil.sanitize(html);
 
-        assertEquals("", sanitized);
-    }
+    assertEquals("", sanitized);
+  }
 
-    @Test
-    void sanitizeNullHtml() {
-        String sanitized = HtmlSanitizerUtil.sanitize(null);
+  @Test
+  void sanitizeNullHtml() {
+    String sanitized = HtmlSanitizerUtil.sanitize(null);
 
-        assertNull(sanitized);
-    }
+    assertNull(sanitized);
+  }
 
-    @Test
-    void sanitizeHtmlWithDisallowedProtocols() {
-        String html = "<a href=\"javascript:alert('XSS')\">Link</a>";
-        String sanitized = HtmlSanitizerUtil.sanitize(html);
+  @Test
+  void sanitizeHtmlWithDisallowedProtocols() {
+    String html = "<a href=\"javascript:alert('XSS')\">Link</a>";
+    String sanitized = HtmlSanitizerUtil.sanitize(html);
 
-        assertEquals("<a rel=\"nofollow\">Link</a>", sanitized);
-    }
-
+    assertEquals("<a rel=\"nofollow\">Link</a>", sanitized);
+  }
 }
