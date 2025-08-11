a881f2e15b79cea097eb767e4ab07a69fa8088e6
Prateek Keerthi <prateekkeerthi7@gmail.com>
2025-08-11 14:31:43 -0400

    MAT-9010 fixed displaying of strikethrough in QDM

--- src/main/java/gov/cms/madie/util/HtmlSanitizerUtil.java
+++ src/main/java/gov/cms/madie/util/HtmlSanitizerUtil.java
@@ -10,7 +10,7 @@ public class HtmlSanitizerUtil {
   public static final String UNKNOWN = "UNKNOWN";
   private static final Safelist RICH_TEXT_SAFE_LIST =
       Safelist.basic()
-          .addTags("s", "br", "table", "tbody", "td", "th", "thead", "tr", "col", "colgroup")
+          .addTags("s", "br", "table", "tbody", "td", "th", "thead", "tr", "col", "colgroup", "del")
           .addAttributes("table", "style", "class", "id")
           .addAttributes("th", "rowspan", "colspan", "style", "colwidth")
           .addAttributes("td", "rowspan", "colspan", "style", "colwidth")
